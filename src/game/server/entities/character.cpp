97a14bd6e1005683a37b0e8d1faeb0c477074dfc
qxdFox <jusi.p@icloud.com>
2025-11-30 01:10:20 +0100

    change predpos

--- src/game/server/entities/character.cpp
+++ src/game/server/entities/character.cpp
@@ -3756,7 +3756,7 @@ vec2 CCharacter::GetPredictedPos(vec2 Pos, vec2 PrevPos, bool Pickup)
 		return Pos;
 
 	double Pred = GetPlayer()->GetClientPred();
-	float dist = distance(Pos, PrevPos) * (Pickup ? 0.5f : 0.3f); // Lasers don't interpolate client side
+	float dist = distance(Pos, PrevPos) * (Pickup ? 0.5f : 0.35f); // Only Pickups interpolate
 	vec2 Dir = normalize(Pos - PrevPos);
 	vec2 nVel = Dir * Pred * dist;
 	return Pos + nVel;
