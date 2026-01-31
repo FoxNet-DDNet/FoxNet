1ba28de912f14e3a613a42ec6a80d6b5c3219c4b
qxdFox <jusi.p@icloud.com>
2026-02-01 00:41:37 +0100

    Add CLIENT_ID_SCRIPTING to ensure no recursion happens

--- src/engine/server/server.cpp
+++ src/engine/server/server.cpp
@@ -748,7 +748,7 @@ int CServer::GetAuthedState(int ClientId) const
 	if(ClientId == IConsole::CLIENT_ID_NO_GAME)
 		return AUTHED_ADMIN;
 	// <FoxNet
-	if(ClientId == IConsole::CLIENT_ID_FIFO || ClientId == IConsole::CLIENT_ID_ECON)
+	if(ClientId == IConsole::CLIENT_ID_FIFO || ClientId == IConsole::CLIENT_ID_ECON || ClientId == IConsole::CLIENT_ID_SCRIPTING)
 		return AUTHED_ADMIN;
 	// FoxNet>
 	dbg_assert(ClientId >= 0 && ClientId < MAX_CLIENTS, "Invalid ClientId: %d", ClientId);
