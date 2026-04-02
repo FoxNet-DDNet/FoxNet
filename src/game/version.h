5f9febb5a57e63b5579d9f747be84231bacf8937
qxdFox <jusi.p@icloud.com>
2026-04-02 12:29:24 +0200

    bump version for minor bug fixes

--- src/game/version.h
+++ src/game/version.h
@@ -18,8 +18,8 @@ extern const char *GIT_SHORTREV_HASH;
 #define GAME_NETVERSION "0.6 626fce9a778df4d4"
 #define GAME_NETVERSION7 "0.7 802f1be60a05665f"
 
-#define FOXNET_VERSION "v3.2"
+#define FOXNET_VERSION "v3.2.1"
 // v16.3.1 = 16031, v8.6.4 = 8064
-#define FOXNET_VERSION_NUMBER 3020
+#define FOXNET_VERSION_NUMBER 3021
 
 #endif
