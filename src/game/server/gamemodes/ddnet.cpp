56682f3b5dde45581274beaccbf9bc364feb3462
qxdFox <jusi.p@icloud.com>
2026-04-03 16:31:59 +0200

    fix spawnsolo

--- src/game/server/gamemodes/ddnet.cpp
+++ src/game/server/gamemodes/ddnet.cpp
@@ -107,17 +107,23 @@ void CGameControllerDDNet::HandleCharacterTiles(CCharacter *pChr, int MultiMapId
 	}
 
 	// solo part
-	if(((TileIndex == TILE_SOLO_ENABLE) || (TileFIndex == TILE_SOLO_ENABLE)) && !Teams().m_Core.GetSolo(ClientId))
+	if((TileIndex == TILE_SOLO_ENABLE) || (TileFIndex == TILE_SOLO_ENABLE))
 	{
 		pChr->UnSpawnSolo(false);
-		GameServer()->SendChatTarget(ClientId, "You are now in a solo part");
-		pChr->SetSolo(true);
+		if(!Teams().m_Core.GetSolo(ClientId))
+		{
+			GameServer()->SendChatTarget(ClientId, "You are now in a solo part");
+			pChr->SetSolo(true);
+		}
 	}
-	else if(((TileIndex == TILE_SOLO_DISABLE) || (TileFIndex == TILE_SOLO_DISABLE)) && Teams().m_Core.GetSolo(ClientId))
+	else if((TileIndex == TILE_SOLO_DISABLE) || (TileFIndex == TILE_SOLO_DISABLE))
 	{
 		pChr->UnSpawnSolo(false);
-		GameServer()->SendChatTarget(ClientId, "You are now out of the solo part");
-		pChr->SetSolo(false);
+		if(Teams().m_Core.GetSolo(ClientId))
+		{
+			GameServer()->SendChatTarget(ClientId, "You are now out of the solo part");
+			pChr->SetSolo(false);
+		}
 	}
 }
 
